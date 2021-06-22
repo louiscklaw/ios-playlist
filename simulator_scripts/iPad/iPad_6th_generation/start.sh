057e3c042da4891f74531fa7411fea29d8581a19
louiscklaw <louiscklaw@gmail.com>
2021-06-22 16:22:53 +0800

    update and test,

--- simulator_scripts/iPad/iPad_6th_generation/start.sh
+++ simulator_scripts/iPad/iPad_6th_generation/start.sh
@@ -6,12 +6,15 @@ set -ex
 
 # xcrun simctl list devicetypes |grep -i ipad
 
-xcrun simctl shutdown iPad-6th-generation || true
-xcrun simctl erase iPad-6th-generation || true
-xcrun simctl delete iPad-6th-generation || true
+# 1=ipad6-portrait
 
-xcrun simctl create iPad-6th-generation com.apple.CoreSimulator.SimDeviceType.iPad--6th-generation-
-xcrun simctl boot iPad-6th-generation
+
+xcrun simctl shutdown $1 || true
+xcrun simctl erase $1 || true
+xcrun simctl delete $1 || true
+
+xcrun simctl create $1 com.apple.CoreSimulator.SimDeviceType.iPad--6th-generation-
+xcrun simctl boot $1
 
 # sleep 30 && xcrun simctl openurl iPad-7th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
 # sleep 30 && xcrun simctl openurl iPad-6th-generation http://192.168.88.254:8002
