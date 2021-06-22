9fa358503c9ae0fb44700ff6b9da338d780b4047
louiscklaw <louiscklaw@gmail.com>
2021-06-22 13:35:23 +0800

    update and test,

---
+++ iPad/iPad_6th_generation/start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# xcrun simctl list |grep -i boot
+
+# xcrun simctl list devicetypes |grep -i ipad
+
+xcrun simctl shutdown iPad-6th-generation || true
+xcrun simctl erase iPad-6th-generation || true
+xcrun simctl delete iPad-6th-generation || true
+
+xcrun simctl create iPad-6th-generation com.apple.CoreSimulator.SimDeviceType.iPad--6th-generation-
+xcrun simctl boot iPad-6th-generation
+
+sleep 20 && xcrun simctl openurl iPad-6th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
+
+# xcrun simctl shutdown iPad-6th-generation
+# xcrun simctl erase iPad-6th-generation
+
+
+# /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator -CurrentDeviceUDID 47947820-FEB1-4E56-9060-F30967F7EEF9
+
+# xcrun simctl list
--- iPad/iPad_7th_generation/start.sh
+++ iPad/iPad_7th_generation/start.sh
@@ -13,7 +13,7 @@ xcrun simctl delete iPad-7th-generation || true
 xcrun simctl create iPad-7th-generation com.apple.CoreSimulator.SimDeviceType.iPad--7th-generation-
 xcrun simctl boot iPad-7th-generation
 
-sleep 5 && xcrun simctl openurl iPad-7th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
+sleep 20 && xcrun simctl openurl iPad-7th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
 
 # xcrun simctl shutdown iPad-7th-generation
 # xcrun simctl erase iPad-7th-generation
