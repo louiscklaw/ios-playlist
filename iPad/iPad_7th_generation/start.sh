a2cb842d8759f97dacfec120cffbee94d2ef6dc4
louiscklaw <louiscklaw@gmail.com>
2021-06-22 13:21:05 +0800

    update and test,

--- iPad/iPad_7th_generation/start.sh
+++ iPad/iPad_7th_generation/start.sh
@@ -6,13 +6,15 @@ set -ex
 
 # xcrun simctl list devicetypes |grep -i ipad
 
-xcrun simctl delete iPad--7th-generation || true
-xcrun simctl create iPad--7th-generation com.apple.CoreSimulator.SimDeviceType.iPad--7th-generation-
+xcrun simctl erase iPad-7th-generation || true
+xcrun simctl shutdown iPad-7th-generation || true
+xcrun simctl delete iPad-7th-generation || true
+xcrun simctl create iPad-7th-generation com.apple.CoreSimulator.SimDeviceType.iPad-7th-generation-
 
-xcrun simctl boot iPad--7th-generation
-xcrun simctl openurl iPad--7th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
-# xcrun simctl shutdown iPad--7th-generation
-# xcrun simctl erase iPad--7th-generation
+xcrun simctl boot iPad-7th-generation
+xcrun simctl openurl iPad-7th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
+# xcrun simctl shutdown iPad-7th-generation
+# xcrun simctl erase iPad-7th-generation
 
 
 # /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator -CurrentDeviceUDID 47947820-FEB1-4E56-9060-F30967F7EEF9
