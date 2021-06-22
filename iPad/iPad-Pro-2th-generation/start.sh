7b7af98f3f10296b3420b2767808fba199a6a74b
louiscklaw <louiscklaw@gmail.com>
2021-06-22 13:49:11 +0800

    update and test,

---
+++ iPad/iPad-Pro-2th-generation/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# xcrun simctl list |grep -i boot
+
+# xcrun simctl list devicetypes |grep -i ipad
+
+xcrun simctl shutdown iPad-Pro-2th-generation || true
+xcrun simctl erase iPad-Pro-2th-generation || true
+xcrun simctl delete iPad-Pro-2th-generation || true
+
+xcrun simctl create iPad-Pro-2th-generation com.apple.CoreSimulator.SimDeviceType.iPad-Pro--11-inch---2nd-generation-
+xcrun simctl boot iPad-Pro-2th-generation
+
+# sleep 30 && xcrun simctl openurl iPad-Pro-2th-generation https://meny-manage--meny-louis-tryout1-4g6dbpux.web.app
+sleep 30 && xcrun simctl openurl iPad-Pro-2th-generation http://192.168.88.254:8002
+
+# xcrun simctl shutdown iPad-Pro-2th-generation
+# xcrun simctl erase iPad-Pro-2th-generation
+
+
+# /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator -CurrentDeviceUDID 47947820-FEB1-4E56-9060-F30967F7EEF9
+
+# xcrun simctl list
